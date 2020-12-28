4863a2d7624094f76dc206a84f84fe1f718a1959
hdorin <hdorin@users.noreply.github.com>
2020-12-28 20:02:33 +0200

    De acum incep sa-l testez

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tensorflow as tf
+from urllib.parse import urlparse
 import numpy as np
 import os
 import sys
 from collections import Counter
+
 
 
 def find_spam_words(words):
@@ -11,25 +13,37 @@ def find_spam_words(words):
             return "SPAM! -" + word
         if "@trash" in word:
             return "SPAM! -" + word
-        if "p://facebook" in word:
+
             return "SPAM! -" + word
-        if "p://bbc" in word:
-            return "SPAM! -" + word
-        if "p://w3" in word or "w3helper." in word:
-            return "SPAM! -" + word
-        if "facebook." in word and not "facebook.com" in word:
-            return "SPAM! -" + word
-        if "@nightmail" in word or ".nightmail" in word:
-            return "SPAM! -" + word
-        if "@nightmail" in word:
-            return "SPAM! -" + word
-        if "$" in word and "000" in word:
-            return "SPAM! -" + word
-        if "livefilestore." in word:
-            return "SPAM! -" + word
+
+
 
 
     return None
+
+def process_words(words):
+    for index, item in enumerate(words):
+        if urlparse(item).netloc:
+            print("DOMAIN " + item)
+            domain = urlparse(item)
+            if '.' in domain.netloc:
+                words[index] = domain.scheme + "://" + domain.netloc.split('.')[-2] + "." +domain.netloc.split('.')[-1]
+                words = words + list(domain.netloc.split('.')[1]+'.')
+            else:
+                words[index] = domain.netloc
+            if "http" not in domain.scheme:
+                words = words + list(domain.scheme + "://")
+                print(">DOMAIN SCHEME " + domain.scheme + "://")
+        elif "$" in item and "000" in item:
+            words[index] = "$$$"
+            print("$$$")
+        elif item.isalpha() == False:
+            str(item).replace('.','')
+            str(item).replace(',', '')
+            str(item).replace('!', '')
+            str(item).replace('?', '')
+            if item.isalpha() == False:
+                words[index] = ' '
 
 def make_dictionary(train_dir):
     emails = [os.path.join(train_dir, f) for f in os.listdir(train_dir)]
@@ -39,23 +53,23 @@ def make_dictionary(train_dir):
             for i, line in enumerate(m):
                 if i == 2:  # Body of email is only 3rd line of text file
                     words = line.split()
+                    process_words(words) #parse URL, remove non-alpha words
                     all_words += words
-                    if  find_spam_words(words) != None:
-                        print(find_spam_words(words))
+                    #if  find_spam_words(words) != None:
+                    #    print(find_spam_words(words))
+
 
 
     dictionary = Counter(all_words)
     list_to_remove = dictionary.keys()
     for item in list(list_to_remove):
-        if item.isalpha() == False and not str(item).endswith(".") and not str(item).endswith(",") \
-                and not str(item).endswith("!") and not str(item).endswith("?"):
-            #print(item)
-            del dictionary[item]
-        elif len(item) == 1:
+        if len(item) == 1:
             del dictionary[item]
 
+
     dictionary = dictionary.most_common(3000)
-    del dictionary[0:25]
+    #del dictionary[0:25]
+    print(dictionary[0:3000])
     return dictionary
 
 
@@ -63,7 +77,7 @@ def extract_features(mail_dir, dictionary):
     files = [os.path.join(mail_dir, fi) for fi in os.listdir(mail_dir)]
     features_matrix = np.zeros((len(files), 3000))
     email_type=np.zeros((len(files)))
-    docID = 0;
+    docID = 0
     fileID=0
     for file in files:
         if file.endswith(".cln"):
@@ -78,12 +92,13 @@ def extract_features(mail_dir, dictionary):
             for i, line in enumerate(fi):
                 if i == 2:
                     words = line.split()
+                    process_words(words)
                     for word in words:
                         wordID = 0
                         for i, d in enumerate(dictionary):
                             if d[0] == word:
                                 wordID = i
-                                features_matrix[docID, wordID] = words.count(word)
+                                features_matrix[docID, wordID] = features_matrix[docID, wordID] + words.count(word)
             docID = docID + 1
 
     return features_matrix, email_type
@@ -114,11 +129,24 @@ elif sys.argv[1] == "train":
 
     dictionary = make_dictionary(sys.argv[2])
     x_train, y_train = extract_features(sys.argv[2], dictionary)
+
     x_train = tf.keras.utils.normalize(x_train, axis=1)
-    print(x_train)
+    print(x_train[5][0:1000])
     #build_model(x_train, y_train)
     # x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+    import xlsxwriter
+
+    workbook = xlsxwriter.Workbook('arrays.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    for row in range(10):
+        for col in range(150):
+            #worksheet.write_column(0, col, x_train[row, col])
+            #if x_train[row,col] > 0:
+            print(str(row) + " " + str(col) + " - " + str(x_train[row, col]))
+
+    workbook.close()
     exit(0)
 
     # f_read.close()
